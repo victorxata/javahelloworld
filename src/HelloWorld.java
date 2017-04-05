495c83dea4628f51ac0b9d1d0b0d68fcb07b6a0a
victorxata <victorxata@gmail.com>
2017-04-05 10:34:28 +0100

    modified source 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
   public static void main (String [] args)
   {
-    System.out.println("hello world from a Java app, modified 2!");
+    System.out.println("hello world from a Java app, modified 3!");
   }
 }
